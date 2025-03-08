9377265b3b2c20efcd25de3314fb48517f8ba180
_VANLI_ <kotovvasa48@gmail.com>
2025-03-08 17:26:27 +0300

    123

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-# token_tg = '7756889405:AAEuzpU-6s7b4lWM9s_qu5SxZZ2h1-JL5ZA'
-token_tg = '8079951249:AAHOJk8B4TivUgE4EWb8Tjx_fB5YnyjgsrA'
+token_tg = '7756889405:AAEuzpU-6s7b4lWM9s_qu5SxZZ2h1-JL5ZA'
+# token_tg = '8079951249:AAHOJk8B4TivUgE4EWb8Tjx_fB5YnyjgsrA'
 token_bs = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6IjkxOGViZjdiLWRhZDctNDVmZC05YzU3LWJlY2RjNDViN2FiNCIsImlhdCI6MTczMzU2NjAzNCwic3ViIjoiZGV2ZWxvcGVyLzRmMWRjYzQ3LWExZjAtNjE1YS1hMWFhLTFlN2U5NmM3Yjc4MSIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiOTIuMjQzLjE4Mi4xNDUiXSwidHlwZSI6ImNsaWVudCJ9XX0.W2iTBntDV8HNvmSGxZR7C3cMvRdcZcemRbAlpSyYFNBYCMp4nWosUmA42FnT_-bwajyDc6618MLxcu1-jUKz4w'
 
 penises = ['ОГОГО!!! ЦЕЛЫХ 100 нанометров\n8=D', 'Ну так себе\n8==D', 'Я пержу дольше\n8===D', 'Яйца\n8====D',
